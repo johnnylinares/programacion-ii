b66c3e2301767b3b6a40bc26eb3cf1b3044f26de
gaam12345 <aguirrementadogabrielandres@gmail.com>
2026-06-03 11:48:49 -0400

    Create godbriel.cpp

---
+++ godbriel.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+/*Imagina que estás organizando un torneo y necesitas administrar a 3 jugadores. De cada 
+jugador necesitas guardar su nombre, su nivel de cuenta y su puntaje final.
+
+Tu programa debe registrar los datos, ordenarlos de mayor a menor puntaje usando la burbuja, 
+y usar una función con apuntadores para decidir si el torneo fue de "Alto Rendimiento"*/
+
+
+
+
+struct jugador {
+    string nombre;
+    int nivel;
+    float pfinal;
+};
+
+
+
+
+
+void burbuja(){
+    
+    
+}
+
+
+
+
+int main(){
+    
+    struct jugador lista[3];
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
